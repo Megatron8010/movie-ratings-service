b5585424dfe1c9e2f0a8877b1c384b03eeb35970
megatron8010 <aditya.singh8010@gmail.com>
2020-04-23 18:14:48 +0530

    ratings service api finished

--- movie-ratings-service/src/main/java/com/example/controller/RaingsResourceController.java
+++ movie-ratings-service/src/main/java/com/example/controller/RaingsResourceController.java
@@ -1,16 +1,32 @@
 package com.example.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.entity.Rating;
+import com.example.entity.UserRating;
 
 @RestController
 @RequestMapping("/ratingsdata")
 public class RaingsResourceController {
-	@RequestMapping("/{movieId}")
-    public Rating getRating(@PathVariable("movieId") String movieId) {
+	@RequestMapping("/movies/{movieId}")
+    public Rating getMovieRating(@PathVariable("movieId") String movieId) {
         return new Rating(movieId, 4);
+    }
+	@RequestMapping("/user/{userId}")
+    public UserRating getUserRatings(@PathVariable("userId") String userId) {
+        List<Rating> ratings = Arrays.asList(
+        		new Rating("1234", 3),
+                new Rating("5678", 4)
+        		);
+        		
+		UserRating userRating = new UserRating();
+        userRating.setRatings(ratings);
+        return userRating;
+
     }
 }
